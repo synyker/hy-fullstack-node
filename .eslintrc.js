f96e6fa3738abf54650bbdbb8c5f27d84ff8683f
Jonne Airaksinen <jonne.airaksinen@perfektio.fi>
2018-02-12 22:11:33 +0200

    part3

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  "env": {
+      "node": true
+  },
+  "parserOptions": {
+      "ecmaVersion": 6
+  },
+  "extends": "eslint:recommended",
+  "rules": {
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "never"
+    ],
+    "eqeqeq": "error",
+    "no-trailing-spaces": "error",
+    "object-curly-spacing": [
+      "error", "always"
+    ],
+    "arrow-spacing": [
+      "error", { "before": true, "after": true }
+    ],
+    "no-console": 0
+  }
+};
